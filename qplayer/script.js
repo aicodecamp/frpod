219aa3610c99a4fd66a5bbe60cb073759cee27f9
Jason <sysbender@gmail.com>
2024-01-18 09:09:13 -0500

    playq update

--- qplayer/script.js
+++ qplayer/script.js
@@ -5,15 +5,15 @@ const player = videojs("myvideo", {
   //   loop: true,
   //fluid, aspectRatio,
   //   playbackRates: [0.25, 0.5, 1, 1.5],
-  plugins: {
-    hotkeys: {
-      volumeStep: 0.1,
-      seekStep: 5,
-      enableModifiersForNumbers: false,
-    },
-  },
+  // plugins: {
+  //   hotkeys: {
+  //     volumeStep: 0.1,
+  //     seekStep: 5,
+  //     enableModifiersForNumbers: false,
+  //   },
+  // },
 });
-
+/*
 player.playlist([
   {
     sources: [
@@ -61,6 +61,21 @@ player.playlist([
     poster: "http://media.w3.org/2010/05/video/poster.png",
   },
 ]);
+*/
+
+const tracks = player.textTracks();
+const track0 = tracks[0];
+console.log(" tracks = ", tracks);
+console.log(" track0 = ", track0);
+var aTextTrack = player.textTracks()[0];
+console.aTextTrack.on("loaded", function () {
+  console.log("here it is");
+  cues = aTextTrack.cues();
+  console.log("Ready State", aTextTrack.readyState());
+  console.log("Cues", cues);
+});
+
+aTextTrack.show(); //this method call triggers the subtitles to be loaded and loaded trigger
 
 // Play through the playlist automatically.
 player.playlist.autoadvance(0);
@@ -68,8 +83,8 @@ player.playlist.autoadvance(0);
 // rotate
 player.rotate(player);
 
-player.playbackRate(2);
-player.currentTime(1);
+// player.playbackRate(1);
+// player.currentTime(1);
 
 player.pause();
 player.play();
